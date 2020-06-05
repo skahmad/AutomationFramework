c094d6675f753dd24ce2c1818a6f492fd53fd0db
partha <partha.sen@xant.ai>
2020-06-06 02:07:09 +0530

    browser configuration added 1. browser size config 2. browser name, multiple browser 3. and few methods added

--- src/main/java/light/automation/configuration/BrowserConfiguration.java
+++ src/main/java/light/automation/configuration/BrowserConfiguration.java
@@ -1,4 +1,26 @@
 package light.automation.configuration;
 
 public interface BrowserConfiguration {
+	static enum BrowserSize {
+		Maximize,
+		Minimize,
+		Normal,
+		Custom,
+		FullScreen
+	};
+	
+	static enum BrowserName {
+		Chrome,
+		Firefox,
+		Opera,
+		Safari,
+		Ie
+	};
+	
+	String getBrowserConfigFilePath();
+	BrowserSize getBrowserSize() ;
+	BrowserName getBrowserName() ;
+	String getDriver() ;
+	
+	String rootUrl();
 }
